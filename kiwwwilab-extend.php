d7fb077c847eee5bdb74959cd7abb592b86c6c2b
Kiwwwilab <144903194+kiwwwilab@users.noreply.github.com>
2026-06-04 19:04:24 +0200

    Update kiwwwilab-extend.php

--- kiwwwilab-extend.php
+++ kiwwwilab-extend.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Kiwwwilab Extend
  * Plugin URI:        https://kiwwwilab.com
  * Description:       This plugin extends all functions and blocks for Kiwwwilab themes.
- * Version:           1.0.9
+ * Version:           1.0.10
  * Author:            Laura Agustí
  * Author URI:        https://kiwwwilab.com
  * Text Domain:       kiwwwilab-extend
